b9ce73a3772826abf46293b2f45b992f5a0a8bc0
fedd <fedd@vsetec.com>
2019-06-22 15:03:44 +0300

    Project start

---
+++ sip/src/main/java/com/vsetec/sip/RequestReceived.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2019 fedd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.vsetec.sip;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ *
+ * @author fedd
+ */
+public class RequestReceived extends AbstractMessageReceived implements Request {
+
+    private final String _protocol;
+    private final String _method;
+    private final String _uri;
+
+    public RequestReceived(InputStream source) throws IOException {
+        super(source);
+        String[] split = getFirstLine().split("\\s+");
+        _method = split[0];
+        _uri = split[1];
+        _protocol = split[2];
+    }
+
+    @Override
+    public String getProtocol() {
+        return _protocol;
+    }
+
+    @Override
+    public String getMethod() {
+        return _method;
+    }
+
+    @Override
+    public String getUri() {
+        return _uri;
+    }
+
+}
